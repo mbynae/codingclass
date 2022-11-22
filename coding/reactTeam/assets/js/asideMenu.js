acf4ccaa632860008b09e042529cae0626e281ce
mybnae <piowm123@gmail.com>
2022-11-22 20:10:08 +0900

    22.11.22

--- coding/reactTeam/assets/js/asideMenu.js
+++ coding/reactTeam/assets/js/asideMenu.js
@@ -4,6 +4,7 @@ const left = document.querySelector(".left");
 const aside = document.getElementById("aside");
 const regionBox = document.querySelector(".region_box");
 const headerImg = document.querySelector("#header img");
+const scroller = document.querySelector(".scroll");
 
 left.addEventListener("mouseenter", (e) => {
     aside.classList.remove("active");
@@ -17,3 +18,21 @@ left.addEventListener("mouseleave", (e) => {
     headerImg.style.transform = "translate(0, 10px)";
     headerImg.style.transition = "all 0.2s";
 });
+
+window.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+    // scroller.innerText = Math.round(scrollTop);
+    headerSearchBar(scrollTop);
+});
+
+function headerSearchBar(scrollTop) {
+    const headerSearch = document.getElementById("header_search");
+    const roomSearch = document.querySelector("#bannerType .roomSearch");
+    console.log(roomSearch.offsetTop);
+
+    if (scrollTop >= roomSearch.offsetTop * 3) {
+        headerSearch.classList.remove("close");
+    } else {
+        headerSearch.classList.add("close");
+    }
+}
